5938af8f0fd60fbb6cb4d428c977dc9f31903c86
彭浩琛 <peng.haochen@iwhalecloud.com>
2025-08-26 00:09:25 +0800

    2025.08.26

---
+++ phc-ai-code-platform/src/main/java/com/phc/phc_ai_code_platform/PhcAiCodePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.phc.phc_ai_code_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhcAiCodePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhcAiCodePlatformApplication.class, args);
+	}
+
+}
