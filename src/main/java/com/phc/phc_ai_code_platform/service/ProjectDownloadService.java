1ff22a56eef9faf82e7676bb696d9d37715d9d62
彭浩琛 <peng.haochen@iwhalecloud.com>
2026-02-04 17:22:31 +0800

    add 下载代码功能后端开发

---
+++ src/main/java/com/phc/phc_ai_code_platform/service/ProjectDownloadService.java
@@ -0,0 +1,9 @@
+package com.phc.phc_ai_code_platform.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+
+}
