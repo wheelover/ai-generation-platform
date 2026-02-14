1c21bef5064b1eb395b4b411273522a7060e36bc
彭浩琛 <peng.haochen@iwhalecloud.com>
2026-02-14 18:03:54 +0800

    update 实时性能优化-同步打包

--- src/main/java/com/phc/phc_ai_code_platform/PhcAiCodePlatformApplication.java
+++ src/main/java/com/phc/phc_ai_code_platform/PhcAiCodePlatformApplication.java
@@ -4,9 +4,11 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableCaching
 @MapperScan("com.phc.phc_ai_code_platform.mapper")
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class}, scanBasePackages = {"com.phc.phc_ai_code_platform", "langgraph4j"})
 public class PhcAiCodePlatformApplication {
